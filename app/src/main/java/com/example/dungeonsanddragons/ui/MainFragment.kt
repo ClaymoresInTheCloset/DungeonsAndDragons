f5a437c13e69bf7bd33c94f00c7ea596e77301e9
Daniel <claymoresinthecloset@gmail.com>
2021-09-03 01:12:58 -0400

    Initial commit

---
+++ app/src/main/java/com/example/dungeonsanddragons/ui/MainFragment.kt
@@ -0,0 +1,20 @@
+package com.example.dungeonsanddragons.ui
+
+import androidx.fragment.app.Fragment
+
+/*
+    DnD note taking app
+    virtual dm screen
+    notes for each catagory (characters, PCs, location)
+    Organize by game
+    hotlinking
+
+
+ */
+class MainFragment: Fragment() {
+
+
+
+
+}
+
